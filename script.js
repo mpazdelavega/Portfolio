76a19fd48e8c7120479be0abfaa70b903e04f8d9
mpazdelavega <72899415+mpazdelavega@users.noreply.github.com>
2025-03-07 22:55:13 -0300

    1.1

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function openCoffeeInvitation() {
+    const newWindow = window.open('', '_blank');
+    newWindow.document.write('<h1 style="text-align: center; margin-top: 20%;">Invitación para un café ☕</h1>');
+    newWindow.document.title = "Invitación";
+  }
